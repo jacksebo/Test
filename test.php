b42c9c4c9d0071eb505db729fc35ef7b2c1352a8
Sebastijan Zajc <sebastijan.zajc@siol.net>
2012-10-10 16:16:02 +0200

    Test
    Test

---
+++ test.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<a href="index.php">Home</a> - 
+<a href="about.php">About Us</a> - 
+<a href="links.php">Links</a> - 
+<a href="contact.php">Contact Us</a> <br />
+<p>This is my home page that uses a common menu to save me time when I add
+new pages to my website!</p>
+</body>
+</html>
